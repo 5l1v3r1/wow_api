32b6be9e22fdd85adc7a2d4b71c62ee6ee1b9c93
Leon V <33411715+leonv024@users.noreply.github.com>
2018-08-24 15:01:56 +0200

    bug fix

--- core/realm_list.py
+++ core/realm_list.py
@@ -2,11 +2,11 @@ import requests, sys, datetime
 from tqdm import tqdm
 from prettytable import PrettyTable
 
-def realm_list(token, api_url):
+def realm_list(token, api_url, realm_status):
     start = datetime.datetime.now()
     r = 0
 
-    table = PrettyTable(['Name', 'id', 'Timezone', 'Status', 'Has que']) # Header
+    table = PrettyTable(['Name', 'id', 'Connected realm','Timezone', 'Status', 'Population', 'Has que']) # Header
     table.align = "l" # Text Align left
 
     headers = {"Authorization": "Bearer %s" % token}
@@ -18,24 +18,33 @@ def realm_list(token, api_url):
         for realm in range(len(data['realms'])):
             name = data['realms'][int(realm)]['name']
             id = data['realms'][int(realm)]['id']
+            connected_realm = data['realms'][int(realm)]['key']['href'].split('/')[-1].split('?')[0]
 
-            try:
-                # Gather realm data such as que, timezone and more
-                url2 = api_url + 'data/wow/connected-realm/%i?namespace=dynamic-eu&locale=en_GB' % id
-                a = requests.get(url2, headers=headers)
-                data2 = a.json()
-                que = data2['has_queue']
-                status = data2['status']['type']
-                population = data2['population']['type']
-                timezone = data2['realms'][0]['timezone']
+            if realm_status == True:
+                try:
+                    # Gather realm data such as que, timezone and more
+                    url2 = api_url + 'data/wow/connected-realm/%i?namespace=dynamic-eu&locale=en_GB' % id
+                    a = requests.get(url2, headers=headers)
+                    data2 = a.json()
+                    que = data2['has_queue']
+                    status = data2['status']['type']
+                    population = data2['population']['type']
+                    timezone = data2['realms'][0]['timezone']
 
-            except Exception as e:
-                que = 'Error'
-                status = 'Error'
-                population = 'Error'
-                timezone = 'Error'
+                except Exception as e:
+                    que = 'Error'
+                    status = 'Error'
+                    population = 'Error'
+                    timezone = 'Error'
+            else:
+                que = 'No data'
+                status = 'No data'
+                population = 'No data'
+                timezone = 'No data'
 
-            table.add_row([name, id, timezone, population, status, que])
+
+            table.add_row([name, id, connected_realm, timezone, population, status, que])
+            table.sortby = "Name"
             r+=1
 
         print(table)
--- core/realm_status.py
+++ core/realm_status.py
@@ -6,13 +6,13 @@ def realm_status(token, api_url, realm):
     start = datetime.datetime.now()
     r = 0
 
-    table = PrettyTable(['Name', 'id', 'Timezone', 'Category', 'Type']) # Header
+    table = PrettyTable(['Name', 'id', 'Connected realm', 'Timezone', 'Category', 'Type', 'Has queue', 'Population']) # Header
     table.align = "l" # Text Align left
 
     headers = {"Authorization": "Bearer %s" % token}
+    # Url for name search and some other data
     url = api_url + 'data/wow/realm/%s?namespace=dynamic-eu&locale=en_GB' % realm
     c = requests.get(url, headers=headers)
-
 
     if c.status_code == 200:
         data = c.json()
@@ -21,19 +21,30 @@ def realm_status(token, api_url, realm):
             # Gather realm data such as que, timezone and more
             name = data['name']
             id = data['id']
+            connected_realm = data['connected_realm']['href'].split('/')[-1].split('?')[0]
             type = data['type']['name']
             category = data['category']
             timezone = data['timezone']
 
+            # Url for realm que and population status and more...
+            url2 = api_url + 'data/wow/connected-realm/' + connected_realm + '?namespace=dynamic-eu&locale=en_GB'
+            b = requests.get(url2, headers=headers)
+            data2 = b.json()
+            que = data2['has_queue']
+            population = data2['population']['name']
+
         except Exception as e:
+            print(e)
             que = 'Error'
             type = 'Error'
             timezone = 'Error'
             category = 'Error'
             id = 'Error'
+            que = 'Error'
+            population = 'Error'
             name = realm
 
-        table.add_row([name, id, timezone, category, type])
+        table.add_row([name, id, connected_realm, timezone, category, type, que, population])
 
         print(table)
     else:
